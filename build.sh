dc9e0e11341bcf5776cbf89e0200353ec1572720
ricardoautomacao10-svg <ricardo.automacao10@gmail.com>
2025-08-23 14:06:47 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
-
-echo "--- Iniciando processo de build simplificado ---"
-
-# 1. Instalação do FFmpeg
-echo "--> Instalando FFmpeg..."
 apt-get update && apt-get install -y ffmpeg
-echo "--> FFmpeg instalado com sucesso."
-
-# 2. Instalação das dependências Python
-echo "--> Instalando pacotes Python..."
 pip install -r requirements.txt
-
-echo "--- Build finalizado com sucesso! ---"
